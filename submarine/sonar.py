e71c1097655dabedfbc6bae8577be6a8fdd2b0c1
David Coopmann <david.coopmann@gmail.com>
2021-12-01 23:59:16 +0100

    puzzle one part two

--- submarine/sonar.py
+++ submarine/sonar.py
@@ -1,4 +1,5 @@
 import typing
+import logging
 
 def import_measurement(file) -> typing.List[int]:
     measurement = []
@@ -10,14 +11,26 @@ def import_measurement(file) -> typing.List[int]:
 
 def sonar_sweep(depth_measurement:typing.List[int])-> int:
     depth_increased_times = 0
-    current_depth = depth_measurement.pop(0)
-
-    for measurement in depth_measurement:
-        if current_depth < measurement:
-            depth_increased_times += 1
-        current_depth = measurement
+    current_depth_window = depth_measurement[0] + depth_measurement[1] + depth_measurement[2]
+    logging.debug(f"initial depth_measurement: {depth_measurement}")
+    logging.debug(f"initial current_depth_window: {current_depth_window}")
     
+    depth_measurement.pop(0) # Remove since only relevant for first measurement window
+    logging.debug(f"after pop() depth_measurement: {depth_measurement}")
+    
+    for measurement in enumerate(depth_measurement):
+        logging.debug(f"measurement: {measurement}")
+        try:
+            window = depth_measurement[measurement[0]] + depth_measurement[measurement[0] + 1] + depth_measurement[measurement[0] + 2]
+            logging.debug(f"window: {window}")
+            if current_depth_window < window:
+                depth_increased_times += 1
+            current_depth_window = window
+        except IndexError:
+            logging.debug("ignore sliding windows with not enough measurements")
+
     return depth_increased_times
 
 if __name__ == "__main__":
-    print(import_measurement("sonar_measurement.txt"))
+    input = import_measurement("sonar_measurement.txt")
+    print(sonar_sweep(input))
--- submarine/test_submarine.py
+++ submarine/test_submarine.py
@@ -3,7 +3,7 @@ import sonar
 example_depths=sonar.import_measurement("test_measurements.txt")
 
 def test_sonar_sweep():
-    assert sonar.sonar_sweep(example_depths) == 7
+    assert sonar.sonar_sweep(example_depths) == 5
 
 def test_pytest():
     assert True
