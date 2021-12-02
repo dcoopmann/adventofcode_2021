ca2fbea74fa8de44493ea5ee5219e07856e67d6c
David Coopmann <david.coopmann@gmail.com>
2021-12-02 21:03:39 +0100

    rename example data for first puzzle

--- submarine/test_submarine.py
+++ submarine/test_submarine.py
@@ -1,6 +1,6 @@
 import sonar
 
-example_depths=sonar.import_measurement("test_measurements.txt")
+example_depths=sonar.import_measurement("test_sonar_measurements.txt")
 
 def test_sonar_sweep():
     assert sonar.sonar_sweep(example_depths) == 5
